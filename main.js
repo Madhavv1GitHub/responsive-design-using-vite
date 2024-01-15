2e5622f91293a7639a5a39aa539f34201bdc7f48
Madhavv <madhavvviswanath@gmail.com>
2024-01-15 11:15:25 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+import './style.css'
+document.querySelector('body').innerHTML = 
+`
+<div class="cover">
+        <div class="navbar">
+            <div class="logo">ART JUNKS</div>
+            <div class="links">
+                <ul>
+                    <li>Home</li>
+                    <li>Start</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+            <div class="nav-icon">
+                <img src="Hamburger Menu.svg" alt="menu" >
+            </div>
+        </div>
+        <div class="main-txt">
+            <input type="search" height="15px" width="90px">
+            <h2 class="cover-head">Art pieces made easy accessible for everyone</h2>
+            <p class="cover-para">Learn, buy and trade art pieces of your favorite artist around the world by using crypto currencies.</p>
+            <button>Learn more</button>
+        </div>
+    </div>
+    
+    <div class="cards">
+        <div class="card1">
+            <img src="card1.jpg" alt="">
+            <h4>5 August,2022</h4>
+            <h3>France seeks to protect Russia's Morozov collection from seizure as masterpieces return home</h3>
+            <p>Read more -></p>
+        </div>
+        <div class="card1">
+            <img src="card2.jpg" alt="">
+            <h4>7 September,2021</h4>
+            <h3>Picasso’s muse as sea creature—will it break $100m at Sotheby's New York sale?</h3>
+            <p>Read more -></p>
+        </div>
+        <div class="card1">
+            <img src="card3.jpg" alt="">
+            <h4>5 August,2022</h4>
+            <h3>Venice Biennale 2022: all the national pavilions, artists and curators</h3>
+            <p>Read more -></p>
+        </div>
+    </div>
+    <footer>
+        <br><br>
+    </footer>
+    
+`
+
+
